da236a1372df0f6d30aee09605e431fd3c8d3dc7
Weslley de Campos <weslleydrum@hotmail.com>
2021-02-21 14:52:43 -0300

    enviando carregamento da pagina de categorias

---
+++ js/category.js
@@ -0,0 +1,42 @@
+const loadProductsWithCategorized = (categoryId) =>{
+  const featuredProducts = document.getElementById('featured-product');
+  const filteredCategories = dataProducts.filter(dataProduct => 
+      dataProduct.categoryId === parseInt(categoryId)
+    );
+  let categoryProducts = '';
+  console.log(dataProducts);
+  filteredCategories.forEach(filtered => {
+    categoryProducts += `
+    <div class="product-card">
+    <div class="box-image-product">
+      <img src="${filtered.image}" alt="Imagem de perfil da pessoa">
+    </div>
+    
+    <div class="box-infos-product">
+      <div class="box-textual">
+        <span class="name-product">${filtered.name}</span>
+        <span class="description-product">
+        ${filtered.description}
+        </span>
+      </div>
+    
+      <div class="box-price">
+      ${
+        filtered.promotion ? `
+        <span class="price-product-has-discount"><span class="monetary-text">R$</span> ${filtered.price}</span>
+        <span class="price-product-discount"><span class="monetary-text">R$</span> ${filtered.priceWithDiscount}</span>
+        ` : `
+        <span class="price-product"><span class="monetary-text">R$</span> ${filtered.price}</span>
+        `
+      }            
+      </div>
+    </div>
+    
+    <div onclick=addProductToShoppingCart("${filtered.id}") class="box-add-cart">
+      <i class="icon-add-cart fas fa-cart-plus"></i>
+    </div>
+  </div>
+    `
+  });
+  featuredProducts.innerHTML = categoryProducts;
+}
--- js/global.js
+++ js/global.js
@@ -10,86 +10,25 @@ const loadCategories = () => {
   menu.innerHTML += menuLoaded;
 };
 
-const loadProducts = () => {
-  const featuredProducts = document.getElementById('featured-product');
-  const promotionalProducts = document.getElementById('promotional')
-
-  let featured = '';
-  let promotional = '';
-
-  dataProducts.forEach((product) => {
-    if(product.featuredProduct){
-      featured += `
-        <div class="product-card">
-          <div class="box-image-product">
-            <img src="${product.image}" alt="Imagem de perfil da pessoa">
-          </div>
-          
-          <div class="box-infos-product">
-            <div class="box-textual">
-              <span class="name-product">${product.name}</span>
-              <span class="description-product">
-              ${product.description}
-              </span>
-            </div>
-          
-            <div class="box-price">
-            ${
-              product.promotion ? `
-              <span class="price-product-has-discount"><span class="monetary-text">R$</span> ${product.price}</span>
-              <span class="price-product-discount"><span class="monetary-text">R$</span> ${product.priceWithDiscount}</span>
-              ` : `
-              <span class="price-product"><span class="monetary-text">R$</span> ${product.price}</span>
-              `
-            }            
-            </div>
-          </div>
-          
-          <div onclick=addProductToShoppingCart("${product.id}") class="box-add-cart">
-            <i class="icon-add-cart fas fa-cart-plus"></i>
-          </div>
-        </div>
-      `;
-    }
-
-    promotional = `
-    <div class="product-card">
-      <div class="box-image-product">
-        <img src="${product.image}" alt="Imagem de perfil da pessoa">
-      </div>
-
-      <div class="box-infos-product">
-        <div class="box-textual">
-          <span class="name-product">${product.name}</span>
-          <span class="description-product">
-            ${product.description}
-          </span>
-        </div>
-
-        <div class="box-price">
-          <span class="price-product-has-discount"><span class="monetary-text">R$</span> ${product.price}</span>
-          <span class="price-product-discount"><span class="monetary-text">R$</span> ${product.priceWithDiscount}</span>
-        </div>
-      </div>
-
-      <div onclick=addProductToShoppingCart("${product.id}") class="box-add-cart">
-        <i class="icon-add-cart fas fa-cart-plus"></i>
-      </div>
-    </div>
-    `;
-
-  });
-
-  featuredProducts.innerHTML += featured;
-  promotionalProducts.innerHTML+= promotional;
-};
-
-window.addEventListener('load', () => {
+const loadShoppingCart = () =>{
   const shoppingCart = '@shopping_cart';
-  loadCategories();
-  loadProducts();
   const products = JSON.parse(localStorage.getItem(shoppingCart) || '[]');
   document.getElementById('count-cart-item').innerHTML = products.length;
+}
+
+
+window.addEventListener('load', () => {
+ 
+  loadCategories();
+  const location =  window.location.search.replace('?', '');
+  
+  if(location === ''){
+    loadProducts();
+  }else{
+    const [, id] = location.split('=');
+    loadProductsWithCategorized(id);
+  }
+  loadShoppingCart();
 });
 
 
